301f5582419c6584e209b2ffdbdf15f811c17128
wento <florea_andreiwento@yahoo.com>
2023-01-22 20:36:07 -0600

    added curent date,month and year

--- assets/script.js
+++ assets/script.js
@@ -3,15 +3,29 @@ var city  = "Kansas City"
 var cityList = $("#city-list");
 
 // add curent date //
-function FormatDay(date){
-    var date = new Date();
-    console.log(date);
-    var month = date.getMonth()+1;
-    var day = date.getDate();
+// function FormatDay(date){
+//     var date = new Date();
+//     console.log(date);
+//     var month = date.getMonth()+1;
+//     var day = date.getDate();
     
-    var dayOutput = date.getFullYear() + '/' +
-        (month<10 ? '0' : '') + month + '/' +
-        (day<10 ? '0' : '') + day;
-    return dayOutput;
-}
+//     var dayOutput = date.getFullYear() + '/' +
+//         (month<10 ? '0' : '') + month + '/' +
+//         (day<10 ? '0' : '') + day;
+//     return dayOutput;
 
+//
+var weatherUrl = `https://api.openweathermap.org/data/2.5/weather?appid=${key}&q=Kansas+City&units=imperial`;
+fetch (weatherUrl).then(response=> {
+    return response.json()
+}) .then(data=> {
+    console.log(data)
+    displyCurentWeather(data)
+})
+
+//Added curent date/month/year
+function displyCurentWeather(data) {
+$(".cardTodayDate").text(dayjs.unix(data.dt).format("M/D/YYYY"))
+
+
+}
