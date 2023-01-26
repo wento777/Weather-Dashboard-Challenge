6d55729ccd34786806529ea697c1031c00a4820a
wento <florea_andreiwento@yahoo.com>
2023-01-26 01:19:42 -0600

    added more cod added five day forecast

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,9 @@
+
+
+
 var key = "71ab138a380e407b27c85c82c67d4dda";
 var city  = "Kansas City"
+var button = document.getElementById("button-addon2");
 
 
 // add curent date //
@@ -43,3 +47,57 @@ icon.setAttribute("src", iconUrl)
 $(".cardBodyToday").append(icon, tempEl,humidityEl,windEl,);
 
 }
+getFiveDayForecast();
+
+
+
+var fiveForecastEl = $('.fiveForecast');
+
+function getFiveDayForecast() {
+	var fiveforcast = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=imperial&appid=${key}`;
+
+    fetch (fiveforcast).then(response=> {
+        return response.json()
+    }) .then(data=> {
+        console.log(data)
+        // displyFiveforcast(data)
+
+        function forecast5(){
+            var fivedayforcast= `https://api.openweathermap.org/data/2.5/forecast?appid=${key}&q=${input}&untis=imperial`; 
+            fetch (fivedayforcast).then(response=> {
+                return response.json()
+            }) .then(data=> {
+                console.log(data)
+                displyFiveDay(data)
+            })
+            }
+            
+            function fivedayforcast(data){
+            
+            
+                for(var i=0; i < 5; i++){
+                $(".fiveForecast").text(dayjs.unix(data[i].dt).format("M/D/YYYY"))
+       
+	
+
+            
+    let tempEl = document.createElement("h2");
+    let humidityEl = document.createElement("h2");
+    let windEl = document.createElement("h2");
+    tempEl.textContent = `Temperature: ${data[i].main.temp} °F`
+    humidityEl.textContent = `Humidity: ${data[i].main.humidity} % `
+    windEl.textContent = ` Wind Speed: ${data[i].wind.speed} MPH`
+    let icon = document.createElement("img")
+    var iconUrl = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+    icon.setAttribute("src", iconUrl)
+    
+    $(".fiveForecast").append(icon, tempEl,humidityEl,windEl,);
+    }
+}
+    })
+}
+$(button).on("click", function(){
+    console.log(input);
+    currentweather();
+    forecast5();
+})
